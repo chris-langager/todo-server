135ae941acade0d8e1500634b49bee982cef217b
Chris Langager <christopher.langager@gmail.com>
2020-01-06 00:49:28 -0700

    bitwise operators working in sql

--- src/queue.ts
+++ src/queue.ts
@@ -3,14 +3,13 @@ import { promisify } from 'util';
 const sleep = promisify(setTimeout);
 
 import * as pgPromise from 'pg-promise';
-import pg = require('pg-promise/typescript/pg-subset');
 const pgp = pgPromise();
 const QueryResultError = pgp.errors.QueryResultError;
 
 const get_next_even_query = `
 select *
 from event_log
-where consumed_by = 0
+where consumed_by & (select id from consumers where name = $1) = 0
 order by id ASC
 limit 1
 for update skip locked;
@@ -18,8 +17,8 @@ for update skip locked;
 
 const commit_id_query = `
 update event_log
-set consumed_by=1
-where id = $1;
+set consumed_by=(consumed_by::bit(16)  | (select id from consumers where name = $1)::bit(16))::integer
+where id = $2;
  `;
 
 interface Payload {
@@ -38,11 +37,11 @@ async function consume(name: string) {
     await db
       .tx(async tx => {
         try {
-          const { id }: EventRow = await tx.one(get_next_even_query);
+          const { id }: EventRow = await tx.one(get_next_even_query, name);
           console.log(`[${name}] working on event ${id}...`);
           await sleep(1000);
           console.log(`[${name}] done with event ${id}.`);
-          await tx.none(commit_id_query, id);
+          await tx.none(commit_id_query, [name, id]);
           return id;
         } catch (err) {
           if (err instanceof QueryResultError) {
@@ -63,4 +62,8 @@ async function consume(name: string) {
 }
 
 consume('A');
+consume('A');
+consume('A');
 consume('B');
+consume('C');
+consume('C');
